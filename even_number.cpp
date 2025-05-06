9abe838104a4f5da6cfd802b57c945836b928b43
Gabimaru_the_Hollow <chandubhargav5555@gmail.com>
2025-05-06 17:19:00 +0530

    even number

---
+++ even_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // cout<<"hello world bro"<<endl;
+    int number;
+    cin>>number;
+    if(number%2==0){
+        cout<<"even number"<<endl;
+    }
+    else{
+        cout<<"odd number"<<endl;
+
+    }
+    
+    return 0;
+}
